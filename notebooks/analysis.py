6f576d6cf33f5867dc4010c66ef3484da2c3493f
Aluko Oluwadamilola <87522676+damzking@users.noreply.github.com>
2023-04-06 19:08:53 +0000

    final changes

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+
+#Import models from scikit learn module:
+
+col_names = ['age', 'workclass', 'fnlwgt','education', 'education-num', 
+'marital-status', 'occupation', 'relationship', 'race', 'sex',
+'capital-gain','capital-loss', 'hours-per-week','native-country', 'income']
+df = pd.read_csv('atlantis.csv', header=None, names = col_names)
+print(df.head())
